673406934de35b94ffd5977bad4994cd1f777f3f
Clood-Desktop <c.descombes@students.unibe.ch>
2014-04-04 11:58:19 +0200

    ignore file update

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lOpenMeshCored -lOpenMeshToolsd -lGLEW -lGL -lGLU -lX11 -lglut
-
